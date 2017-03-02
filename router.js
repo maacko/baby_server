92079f0d1d0d11e1f1c636093eefb46bbf4cc71b
Marco Siguencia <m08251983@gmail.com>
2017-03-02 16:12:56 -0500

    Adding Comments to router.js

--- router.js
+++ router.js
@@ -1,3 +1,7 @@
+/*Again we want reusability in our router, reusability allows us to perform
+* dependancy injections so that we can lightly couple the server and the
+* router.
+*/
 var route = function (pathname) {
     /*a basic prototype of what route should at take in at the minimum*/
     console.log('Routing' + pathname + 'to the appropriate handler');
