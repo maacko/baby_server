e830dda2459ae41e55d8b25de58a1724fcca943d
Marco Siguencia <m08251983@gmail.com>
2017-03-02 19:22:52 -0500

    Indentation changes for server.js

--- server.js
+++ server.js
@@ -12,35 +12,35 @@ var url = require('url');
 */
 var start = function (route, handle) {
 
-/* This function will be called back to when a request has been received. The
- * request will be passed as an object along with an empty response object, which
- * we can modify.
- */
-var onRequest = function (request, response) {
-    /* Note: If a request is issued from a browser, then this function will be
-     * invoked twice since the browser issues two http requests: one to retrieve
-     * a favicon and another for the user's request.
+    /* This function will be called back to when a request has been received. The
+     * request will be passed as an object along with an empty response object, which
+     * we can modify.
      */
-    console.log('Request Received');
+    var onRequest = function (request, response) {
+        /* Note: If a request is issued from a browser, then this function will be
+         * invoked twice since the browser issues two http requests: one to retrieve
+         * a favicon and another for the user's request.
+         */
+        console.log('Request Received');
 
-    var pathname = url.parse(request.url).pathname;
-    route(pathname, handle);
+        var pathname = url.parse(request.url).pathname;
+        route(pathname, handle);
 
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write('Hello World');
-    response.end();
-}
-/* createServer is an async function, therefore any subsequent code will not be
- * block because of its invocation. Upon a request, the server will make a call back to
- * the function onRequest -- functions like onRequest are referred to as callbacks.
- */
-http.createServer(onRequest).listen(8888);
-console.log('Server Created');
-/* The expected output is:
- * Server Created
- * Request Received <-This output only occurs if the browser (client) makes a request
- * Request Received
- */
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.write('Hello World');
+        response.end();
+    }
+    /* createServer is an async function, therefore any subsequent code will not be
+     * block because of its invocation. Upon a request, the server will make a call back to
+     * the function onRequest -- functions like onRequest are referred to as callbacks.
+     */
+    http.createServer(onRequest).listen(8888);
+    console.log('Server Created');
+    /* The expected output is:
+     * Server Created
+     * Request Received <-This output only occurs if the browser (client) makes a request
+     * Request Received
+     */
 };
 
 /*server is a module, exported as an object, that exposes start as a method*/
